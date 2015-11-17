80bffa4efbc131974eed52c65526422ce29728d6
medmondson <eddy556@gmail.com>
2015-11-17 23:05:35 +0000

    Am I able to commit?

--- Src/CShell.Hosting.Tests/ReplScriptExecutorTests.cs
+++ Src/CShell.Hosting.Tests/ReplScriptExecutorTests.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Versioning;
 using System.Text;
-using ScriptCs.Logging;
+using ScriptCs.Logging; 
 
 namespace CShell.Hosting.Tests
 {
